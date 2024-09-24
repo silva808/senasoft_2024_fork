cb882db23d9bba3771baec93c573fb44d53c1d3b
Jair Silva <jair.monteria.401@gmail.com>
2024-09-24 14:05:28 -0500

    Classes for each table added

--- backend/class/Post.php
+++ backend/class/Post.php
@@ -1,10 +1,10 @@
-<?php 
-class Bike{
+<?php
+class Post{
 
     public $db_connect;
 
     public function __construct($db_connect){
-        
+
         $this -> db_connect = $db_connect;
     }
 }
