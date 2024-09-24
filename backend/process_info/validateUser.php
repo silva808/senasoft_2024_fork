ca917e5b993b7add5f02cbce99135004550be266
Alejandra <alejandra03fajardo@gmail.com>
2024-09-24 18:46:50 -0500

    Validacion user

--- backend/process_info/validateUser.php
+++ backend/process_info/validateUser.php
@@ -1,5 +1,27 @@
 <?php 
-if(){
-    
+session_start();
+require_once '../config/db_connection.php';
+include_once '../class/User.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $id= $_POST["id"];
+    $email= $_POST["email"];
+    $pass= $_POST["pass"];
+
+    //Validar Existencia del usuario
+    if(){
+
+        if($DatosFinca[2] == 7){
+            echo "<script> window.location.href='../../Interfaz/Users/Usuario/usuario.php'</script>";
+        }
+        else if($DatosFinca[2] == 5){
+            echo "<script> window.location='../../Interfaz/Users/Administrador/administrador_design.php'</script>";
+        }
+
+    }else{
+        echo "Usuario no existe";
+    }
+
 }
 ?>
