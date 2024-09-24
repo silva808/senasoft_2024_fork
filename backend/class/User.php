56004371a1d273b3d347ec97471e798892e727db
Alejandra <alejandra03fajardo@gmail.com>
2024-09-24 18:39:48 -0500

    Avances

--- backend/class/User.php
+++ backend/class/User.php
@@ -7,4 +7,33 @@ class User{
 
         $this -> db_connect = $db_connect;
     }
+
+    public function getUser($id){
+        $sql = "SELECT * FROM users WHERE id = ?";
+        $stmt = $this -> db_connect -> prepare($sql);
+        $stmt -> bind_param("i", $id);
+        $stmt -> execute();
+        $result = $stmt -> get_result();
+        $row = $result -> fetch_assoc();
+        return $row;
+    }
+
+    public function validateUser($id, $email, $password){
+        $sql = "SELECT * FROM users WHERE id = ? AND email = ? AND password = ?";
+        $stmt = $this -> db_connect -> prepare($sql);
+        $stmt -> bind_param("iss", $id, $email, $password);
+        $stmt -> execute();
+        if($stmt -> get_result() -> num_rows > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public createUser($name, $email, $phone_number, $password, $economic_id){
+        $sql = "INSERT INTO users (name, email, phone_number, password, economic_id) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this -> db_connect -> prepare($sql);
+        $stmt -> bind_param("ssssi", $name, $email, $phone_number, $password, $economic_id);
+        $stmt -> execute();
+        return true;
+    }
 }
